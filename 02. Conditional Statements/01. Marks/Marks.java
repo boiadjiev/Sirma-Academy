36d853ea400e8e631f37ec85fe5265a68e80e79b
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-09-18 11:52:28 +0300

    Create Marks.java

---
+++ 02. Conditional Statements/01. Marks/Marks.java
@@ -0,0 +1,13 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class Marks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double mark = Double.parseDouble(scanner.nextLine());
+        if (mark >= 5.50) {
+            System.out.println("Excellent!");
+        }
+    }
+}
