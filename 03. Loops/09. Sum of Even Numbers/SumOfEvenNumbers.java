66e61068d9ca4020fd6d89757c674ada458cea3f
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-10-04 10:47:12 +0300

    Create SumOfEvenNumbers.java

---
+++ 03. Loops/09. Sum of Even Numbers/SumOfEvenNumbers.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        for (int i = 1; i <= n ; i++) {
+            sum += 2 * i;
+        }
+
+        System.out.println(sum);
+    }
+}
