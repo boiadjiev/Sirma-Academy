761ee6f616739e46eed9f1498859c5156928d91a
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-10-15 15:49:12 +0300

    Create HalfRhombusFromAsterisks.java

---
+++ 03. Loops/31. Half Rhombus from Asterisks/HalfRhombusFromAsterisks.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class HalfRhombusFromAsterisks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = Integer.parseInt(sc.nextLine());
+        for (int i = 1; i <= input; i++) {
+            System.out.println("* ".repeat(i));
+        }
+
+        for (int j = input - 1; j >= 1; j--) {
+            System.out.println("* ".repeat(j));
+        }
+    }
+}
