6eb348937a148e432e12469967da331a1f0786c9
boiadjiev <123622892+boiadjiev@users.noreply.github.com>
2023-10-27 02:00:47 +0300

    Create PrintEveryNthElementFromAnArray.java

---
+++ 04. Arrays/09. Print Every N-th Element From an Array/PrintEveryNthElementFromAnArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class PrintEveryNthElementFromAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] arr = sc.nextLine().split(",");
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < arr.length; i += n) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
